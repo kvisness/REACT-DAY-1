4d241874e4c287a8ee6fbe9a26c659e31cda8b12
Kris Visness <kvisness@hbm2.com>
2020-09-29 15:16:01 -0500

    completed base goals

---
+++ src/About/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1> About </h1>
+        <p>We are actors working with actors and we react to things.</p>
+      </div>
+    );
+  }
+}
+
+export default About;
